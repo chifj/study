678c7f3c9181ff3986191dadc8c866564578b6c8
Chi, Kevin-FJ <Kevin-FJ.Chi@aia.com>
2020-02-28 15:01:04 +0800

    Update...

--- src/test/java/com/itnet/study/util/LetterCombinationUtilTest.java
+++ src/test/java/com/itnet/study/util/LetterCombinationUtilTest.java
@@ -34,14 +34,18 @@ public class LetterCombinationUtilTest {
         Integer[] input0 = new Integer[]{0, 1, 4};
         Integer[] input1 = new Integer[]{0, 1, 2, 3, 1, 0};
         Integer[] input2 = new Integer[]{4, 23};
+        Integer[] input3 = new Integer[]{2, 3, 4, 01, 10, 11, 00, 20, 99};
+
 
         List<String> output0 = LetterCombinationUtil.letterCombination(input0);
         List<String> output1 = LetterCombinationUtil.letterCombination(input1);
         List<String> output2 = LetterCombinationUtil.letterCombination(input2);
+        List<String> output3 = LetterCombinationUtil.letterCombination(input3);
 
         System.out.println(output0.toString());
         System.out.println(output1.toString());
         System.out.println(output2.toString());
+        System.out.println(output3.toString());
     }
 
 }
